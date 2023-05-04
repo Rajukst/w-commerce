08c40fb2252bbe08dd486248c22c8c7d50afce0b
Rajukst <rajuahamedkst15@gmail.com>
2023-05-04 10:07:30 +0600

    done

--- src/WebPages/Pages/HomeShowingProducts/ShowingProductTwo/ShowingProductTwo.js
+++ src/WebPages/Pages/HomeShowingProducts/ShowingProductTwo/ShowingProductTwo.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import "./ShowingProductTwo.css"
-const ShowingProductTwo = ({productlist}) => {
-    const {name, priceOne, priceTwo, image, _id}= productlist;
-    return (
-        <>
-        <Col>
+import React from "react";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./ShowingProductTwo.css";
+const ShowingProductTwo = ({ productlist }) => {
+  const { name, priceOne, priceTwo, image, _id } = productlist;
+  return (
+    <>
+      <Col>
         <div className="imagBody">
-            <div className="headings">
-                <img className='img-fluid' src={image} alt="" />
+          <div className="headings">
+            <img className="img-fluid" src={image} alt="dynamic-image" />
+          </div>
+          <Link to={`/home-product/${_id}`}>
+            <h6>{name}</h6>
+          </Link>
+          <div className="TextBody">
+            <div className="priceOne">
+              <p className="priceOnes">${priceOne}</p>
             </div>
-           <Link to={`/home-product/${_id}`}> <h6>{name}</h6></Link>
-            <div className="TextBody">
-                <div className="priceOne">
-                    <p className='priceOnes'>${priceOne}</p>
-                </div>
-                <div className="priceOne">
-                    <p>${priceTwo}</p>
-                </div>
+            <div className="priceOne">
+              <p>${priceTwo}</p>
             </div>
-            <div className="reviews">
+          </div>
+          <div className="reviews">
             <i className="fa-regular fa-star"></i>
             <i className="fa-regular fa-star"></i>
             <i className="fa-regular fa-star"></i>
             <i className="fa-regular fa-star"></i>
             <i className="fa-regular fa-star"></i>
-            </div>
+          </div>
         </div>
-        </Col>
-        </>
-        
-        
-    );
+      </Col>
+    </>
+  );
 };
 
 export default ShowingProductTwo;
